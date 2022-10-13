ad632968e0315173a5fdc9076ab0bd53964c1e58
David Redding <redding.david35@gmail.com>
2022-10-12 20:39:06 -0500

    makeStyles/useStyles

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Typography, Button, Container, makeStyles } from '@material-ui/core/';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue',
+    },
+  },
+
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  },
+});
 
 const Create = () => {
-  return <div>Create page</div>;
+  const { btn, title } = useStyles();
+  return (
+    <Container>
+      <Typography variant="h6" color="textSecondary" component="h2" gutterBottom>
+        Create a New Note
+      </Typography>
+      <Button
+        className={btn}
+        type="submit"
+        variant="contained"
+        color="secondary"
+        startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 };
 
 export default Create;
