9500e68c8514bf0f9b0aecc77f83ee3d12ca187b
David Redding <redding.david35@gmail.com>
2022-10-17 12:23:51 -0500

    List + List items

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,13 @@
-import { Drawer, makeStyles, Typography } from '@material-ui/core';
+import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Typography } from '@material-ui/core';
+import { SubjectOutlined, InboxOutlined, DraftsOutlined, AddCircleOutlineOutlined } from '@material-ui/icons/';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
   page: {
     background: '#f9f9f9',
     width: '100%',
@@ -13,13 +18,21 @@ const useStyles = makeStyles({
   drawerPaper: {
     width: drawerWidth,
   },
-  root: {
-    display: 'flex',
+  active: {
+    background: '#f4f4f4',
   },
 });
 
 const Layout = ({ children }) => {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+  console.log(location);
+
+  const menuItems = [
+    { text: 'My Notes', icon: <SubjectOutlined color="secondary" />, path: '/' },
+    { text: 'Create Note', icon: <AddCircleOutlineOutlined color="secondary" />, path: '/create' },
+  ];
 
   return (
     <div className={classes.root}>
@@ -34,8 +47,25 @@ const Layout = ({ children }) => {
         classes={{ paper: classes.drawerPaper }}
       >
         <div>
-          <Typography variant="h5">Ninja Notes</Typography>
+          <Typography variant="h5">JotNotes</Typography>
         </div>
+
+        {/* {list / links} */}
+
+        <List component="nav">
+          {menuItems.map((item) => (
+            <ListItem
+              button
+              key={item.text}
+              className={location.pathname === item.path ? classes.active : null}
+              onClick={() => history.push(item.path)}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
       </Drawer>
 
       <div className={classes.page}>{children}</div>
